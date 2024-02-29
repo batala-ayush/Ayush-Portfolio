e119ffb05e667d2cf02e0df14f9cb82a5817153a
batala-ayush <ayushbatala690@gmail.com>
2024-02-29 11:12:43 +0545

    Final Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,3 @@
-/*
-   
-    Template Name : Rolling - Freelancer Portfolio Template
-    Author : UiPasta Team
-    Website : http://www.uipasta.com/
-    Support : http://www.uipasta.com/support/
-	
-	
-*/
-
-
-
-/*
-   
-   Table Of Content
-   
-   1. Preloader
-   2. Smooth Scroll
-   3. Scroll Naviagation Background Change with Sticky Navigation
-   4. Mobile Navigation Hide or Collapse on Click
-   5. Scroll To Top
-   6. Typed.js
-   7. Parallax Background
-   8. Portfolio Filtering
-   9. Magnific Popup
-  10. Testimonial Carousel/Slider
-  11. Statistics Counter
-  12. Google Map
- 
-
-*/
-
-
 (function ($) {
     'use strict';
 
@@ -134,36 +101,6 @@
 
         $('.portfolio-inner').mixItUp();
 
-
-       
-        /* Magnific Popup */
-
-        // $('.portfolio-popup').magnificPopup({
-        //     type: 'image',
-			
-        //     gallery: { enabled: true },
-		// 	zoom: { enabled: true,
-		// 	        duration: 500
-					
-        //   },
-		  
-        //  image:{
-        //        markup: '<div class="mfp-figure portfolio-pop-up">'+
-        //        '<div class="mfp-close"></div>'+
-        //        '<div class="mfp-img"></div>'+
-        //        '<div class="mfp-bottom-bar portfolio_title">'+
-        //        '<div class="mfp-title"></div>'+
-        //        '<div class="mfp-counter"></div>'+
-        //        '</div>'+
-        //        '</div>',
-
-        //        titleSrc:function(item){
-        //         return item.el.attr('title');
-        //       }
-        //     }
-		  
-		  
-        //   });
 
 
 
